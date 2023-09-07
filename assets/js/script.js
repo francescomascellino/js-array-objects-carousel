558b06cf413e06ff88e8681b307f5136cf3bc213
francescomascellino <franesco.mascellino@gmail.com>
2023-09-07 14:49:06 +0200

    add for in loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,63 +1,54 @@
-/* 
+/* Consegna:
+Riprendiamo il live coding visto in classe un pó di tempo fá sul carosello di immagini e rifacciamolo :gatto_che_urla:, questa volta usando gli oggetti.
+:boolean-hug: Potete prendere come riferimento il codice scritto insieme nel live, che troverete direttamente nella mia repository di github a questo link: [https://github.com/fabiopacifici/104_js/tree/main/live_slider]
 
-Consegna:
-Dato un array contenente una lista di cinque immagini, creare un carosello come nello screenshot allegato.
+Modifichiamo il codice dell'esercizio per renderlo funzionante con un array di oggetti al posto di un array di stringhe.
 
+Bonus 0:
+Non eramamo ancora a conoscenda di molti strumenti utili, come ad esempio le funzioni. É possibile fare refactoring del codice, pulendolo e creando quanche funzione che possa rendere tutto piú leggibile e pulito?
 
-MILESTONE 1
-Per prima cosa, creiamo il markup statico: costruiamo il container e inseriamo un'immagine grande al centro: avremo così la struttura base e gli stili pronti per poterci poi concentrare solamente sull'aspetto logico.
+Bonus 1:
+Sperimentiamo attraverso l'uso delle timing functions anche una funzionalità di scorrimento al nostro carosello:
+al click di un bottone o già dall'inizio possiamo far partire, ad intervalli di tempo a piacere, lo scorrimento delle immagini disponibili nel carosello stesso.
 
-
-MILESTONE 2
-Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. 
-Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-
-
-MILESTONE 3
-Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
-
-
-BONUS 1:
-Aggiungere il ciclo infinito del carosello. Ovvero se è attiva la prima immagine e l'utente clicca la freccia per andare all’immagine precedente, dovrà comparire l’ultima immagine dell’array e viceversa.
-
-
-BONUS 2:
-Aggiungere la visualizzazione di tutte le thumbnails sulla destra dell’immagine grande attiva, come nello screenshot proposto. Tutte le miniature avranno un layer di opacità scura, tranne quella corrispondente all’immagine attiva, che invece avrà un bordo colorato. 
-Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di miniatura attiva.
-
-Prima di partire a scrivere codice:
-Non lasciamoci spaventare dalla complessità apparente dell'esercizio, ma analizziamo prima, come abbiamo fatto sempre, cosa ci potrebbe aspettare. Abbiamo completato ormai da qualche giorno la sessione HTML e CSS, se non ci ricordiamo qualcosa andiamo pure a riguardare alcuni argomenti. Non dedichiamo però al ripasso più di una mezz'ora, così da non perdere di vista il focus dell'esercizio.
-
-Consigli del giorno:
-1. Costruiamo del carosello una versione statica contenente solamente un'immagine. Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi per poterli riprodurre dinamicamente in js. Potremo quindi usarli come "template". 
-2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
-3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa domanda: "Quanti cicli servono?"
-
-Buon lavoro e buon divertimento! :slightly_smiling_face:
-
-
-
-
-MILESTONE 2
-Adesso rimuoviamo tutto il markup statico e inseriamo tutte le immagini dinamicamente servendoci dell'array fornito e un semplice ciclo for che concatena un template literal. 
-
-
-Tutte le immagini saranno nascoste, tranne la prima, che avrà una classe specifica che la renderà visibile.
-
-Al termine di questa fase ci ritroveremo con lo stesso slider stilato nella milestone 1, ma costruito dinamicamente attraverso JavaScript.
-
-
-<img class="active" src="./assets/img/01.webp" alt="">
-*/
+Bonus 2:
+E se volessi un bottone per invertire la "direzione" del carosello? */
 
 /* Define the slides list */
-const slides = [
+/* const slides = [
     './assets/img/01.webp', //0
     './assets/img/02.webp', //1
     './assets/img/03.webp', //etc
     './assets/img/04.webp',
     './assets/img/05.webp',
+] */
+
+const slides = [
+    {
+        path: './assets/img/01.webp',
+        // status: "active",
+    },
+
+    {
+        path: './assets/img/02.webp',
+        // status: "active",
+    },
+
+    {
+        path: './assets/img/03.webp',
+        // status: "active",
+    },
+
+    {
+        path: './assets/img/04.webp',
+        // status: "active",
+    },
+
+    {
+        path: './assets/img/05.webp',
+        // status: "active",
+    }
+
 ]
 
 let activeSlide = 0;
@@ -71,30 +62,44 @@ const nextEl = document.querySelector('.next')
 
 /* Print all images into the dom */
 // loop over the slides 
-for (let i = 0; i < slides.length; i++) {
+
+for (const key in slides) {
+
+    console.log("key", key);
+    const slidePath = slides[key].path;
+    console.log("slidepath", slidePath);
+
+    const slideMarkup = `<img class="${activeSlide == key ? 'active' : ''}" src="${slidePath}" alt="">`;
+    console.log("slide markup", slideMarkup);
+
+    sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup);
+};
+
+
+/* for (let i = 0; i < slides.length; i++) {
 
     const slidePath = slides[i];
     console.log(slidePath);
 
-    /* TERNARY OPERATOR
+    // TERNARY OPERATOR
 
-    if(condition) {
-    // code to run
-    } else {
-    // code to run
-    }
+    // if(condition) {
+    // // code to run
+    // } else {
+    // // code to run
+    // }
 
-    // with ternary operator
+    // // with ternary operator
 
-    condition ? 'code to run' : 'code to run'
-    */
+    // condition ? 'code to run' : 'code to run'
+
 
     //for each slide we create a markup element. if activeSlide is === i it has the active class. This make the code show the first image as for now activeSlide = 0. the other images will be printed on the markup without the "active" class.    
     const slideMarkup = `<img class="${activeSlide === i ? 'active' : ''}" src="${slidePath}" alt="">`
 
     sliderImagesEl.insertAdjacentHTML('beforeend', slideMarkup)
 
-}
+} */
 
 /* 
 MILESTONE 3
@@ -120,7 +125,19 @@ Al click delle frecce, oltre al cambio di immagine attiva, gestire il cambio di 
 const thumbsElement = document.querySelector('.thumbnails')
 
 //Loops over the thumbs and prints them on the DOM
-for (let i = 0; i < slides.length; i++) {
+for (const key in slides) {
+
+    console.log("key", key);
+    const thumbPath = slides[key].path;
+    console.log("slidepath", thumbPath);
+
+    const thumbMarkup = `<img class="${activeSlide == key ? 'active' : ''}" src="${thumbPath}" alt="">`;
+    console.log("slide markup", thumbPath);
+
+    thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup);
+};
+
+/* for (let i = 0; i < slides.length; i++) {
 
     //The thumb path is the same as the slides
     const thumbPath = slides[i];
@@ -130,7 +147,7 @@ for (let i = 0; i < slides.length; i++) {
 
     thumbsElement.insertAdjacentHTML('beforeend', thumbMarkup)
 
-}
+} */
 
 // intercept click on the next icon 
 nextEl.addEventListener('click', function () {
@@ -148,7 +165,7 @@ nextEl.addEventListener('click', function () {
     console.log("currentThumb = ", currentThumb);
 
     // remove the active class from the active thumb
-    currentThumb.classList.remove('active')
+    currentThumb.classList.remove('active');
 
     //after we remove the acrive class from the images we increment the activeSlide value by 1
     if (activeSlide === slidesImages.length - 1) {
@@ -167,14 +184,14 @@ nextEl.addEventListener('click', function () {
     console.log("nextSlide = ", nextSlide);
 
     // add the active class to the next slide
-    nextSlide.classList.add('active')
+    nextSlide.classList.add('active');
 
     // select the next thumb
-    const nextThumb = document.querySelectorAll('.thumb')[activeSlide];
+    const nextThumb = document.querySelectorAll('.thumbnails img')[activeSlide];
     console.log("nextThumb = ", nextThumb);
 
     // add to the next thumb the active class
-    nextThumb.classList.add('active')
+    nextThumb.classList.add('active');
 
 })
 
@@ -196,7 +213,7 @@ prevEl.addEventListener('click', function () {
     console.log("currentThumb = ", currentThumb);
 
     // remove the active class from the active thumb
-    currentThumb.classList.remove('active')
+    currentThumb.classList.remove('active');
 
     //after we remove the acrive class from the images we increment the activeSlide value by 1
     if (activeSlide === 0) {
@@ -219,7 +236,7 @@ prevEl.addEventListener('click', function () {
     nextSlide.classList.add('active')
 
     // select the next thumb to show
-    const nextThumb = document.querySelectorAll('.thumb')[activeSlide];
+    const nextThumb = document.querySelectorAll('.thumbnails img')[activeSlide];
     console.log("nextThumb = ", nextThumb);
 
     // add to the next thumb the active class
